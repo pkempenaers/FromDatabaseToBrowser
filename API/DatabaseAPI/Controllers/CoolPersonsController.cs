040f58bb4cf2edf4a1719798be119e66d7e86f92
pkempenaers <pieter.kempenaers@gmail.com>
2017-01-08 18:04:30 +0100

    Step 5: Remove unused controller methods

--- API/DatabaseAPI/Controllers/CoolPersonsController.cs
+++ API/DatabaseAPI/Controllers/CoolPersonsController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -36,72 +36,6 @@ namespace DatabaseAPI.Controllers
             return Ok(coolPerson);
         }
 
-        // PUT: api/CoolPersons/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutCoolPerson(int id, CoolPerson coolPerson)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != coolPerson.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(coolPerson).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CoolPersonExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/CoolPersons
-        [ResponseType(typeof(CoolPerson))]
-        public async Task<IHttpActionResult> PostCoolPerson(CoolPerson coolPerson)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.CoolPersons.Add(coolPerson);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = coolPerson.Id }, coolPerson);
-        }
-
-        // DELETE: api/CoolPersons/5
-        [ResponseType(typeof(CoolPerson))]
-        public async Task<IHttpActionResult> DeleteCoolPerson(int id)
-        {
-            CoolPerson coolPerson = await db.CoolPersons.FindAsync(id);
-            if (coolPerson == null)
-            {
-                return NotFound();
-            }
-
-            db.CoolPersons.Remove(coolPerson);
-            await db.SaveChangesAsync();
-
-            return Ok(coolPerson);
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -109,11 +43,6 @@ namespace DatabaseAPI.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private bool CoolPersonExists(int id)
-        {
-            return db.CoolPersons.Count(e => e.Id == id) > 0;
         }
     }
 }
